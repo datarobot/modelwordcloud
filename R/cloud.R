3b277f84add8d6b155a87cabdae49f754e9d4cb1
peterhurford <peter@peterhurford.com>
2017-08-10 15:55:55 -0500

    Clean up the code II

--- R/cloud.R
+++ R/cloud.R
@@ -13,7 +13,7 @@
 #' @param bg_color character. The color of the background.
 #' @export
 wordcloud <- function(words, freq, coefficients = NULL, colors = "black",
-                      scale = c(4, .5), min_freq = 3, max_words = Inf,
+                      scale = c(4, 0.5), min_freq = 3, max_words = Inf,
                       random_order = FALSE, random_color = FALSE, rot_per = 0,
                       bg_color = "#FFFFFF") { 
 
@@ -37,14 +37,14 @@ wordcloud <- function(words, freq, coefficients = NULL, colors = "black",
       sw2 <- bnds[3]
       sh2 <- bnds[4]
       if (x1 < x2) {
-        overlap <- x1 + sw1 > x2-s
+        overlap <- x1 + sw1 > x2 - s
       } else {
-        overlap <- x2 + sw2 > x1-s
+        overlap <- x2 + sw2 > x1 - s
       }
       if (y1 < y2) {
-        overlap <- overlap && (y1 + sh1 > y2-s)
+        overlap <- overlap && (y1 + sh1 > y2 - s)
       } else {
-        overlap <- overlap && (y2 + sh2 > y1-s)
+        overlap <- overlap && (y2 + sh2 > y1 - s)
       }
       if(overlap) {
         last <<- i
@@ -83,12 +83,12 @@ wordcloud <- function(words, freq, coefficients = NULL, colors = "black",
     colors <- colors[ord][freq >= min_freq]
   }
   
-  thetaStep <- .1
-  rStep <- .05
+  thetaStep <- 0.1
+  rStep <- 0.05
   plot.new()
   op <- par("mar")
   par(mar = c(0, 0, 0, 0))
-  plot.window(c(0,1), c(0,1), asp = 1)
+  plot.window(c(0, 1), c(0, 1), asp = 1)
   normedFreq <- freq / max(freq)
   size <- (scale[1] - scale[2]) * normedFreq + scale[2]
   boxes <- list()
@@ -102,7 +102,7 @@ wordcloud <- function(words, freq, coefficients = NULL, colors = "black",
     wid <- strwidth(words[i], cex = size[i])
     ht <- strheight(words[i], cex = size[i])
     #mind your ps and qs
-    if (grepl(tails, words[i])) { ht <- ht + ht*.2 }
+    if (grepl(tails, words[i])) { ht <- ht + ht * 0.2 }
     if (rotWord) {
       tmp <- ht
       ht <- wid
@@ -110,9 +110,9 @@ wordcloud <- function(words, freq, coefficients = NULL, colors = "black",
     }
     isOverlaped <- TRUE
     while(isOverlaped) {
-      if (!overlap(x1 - .5 * wid, y1 - .5 * ht, wid, ht) &&
-          x1 - .5 * wid > 0 && y1 - .5 * ht > 0 &&
-          x1 + .5 * wid < 1 && y1 + .5 * ht < 1) {
+      if (!overlap(x1 - 0.5 * wid, y1 - 0.5 * ht, wid, ht) &&
+          x1 - 0.5 * wid > 0 && y1 - 0.5 * ht > 0 &&
+          x1 + 0.5 * wid < 1 && y1 + 0.5 * ht < 1) {
         if (!isTRUE(random_color)) {
           if (!is.null(coefficients)) {
             cc <- colors[[round(coefficients[[i]] * (length(colors) - 1)) + 1]]
@@ -123,17 +123,17 @@ wordcloud <- function(words, freq, coefficients = NULL, colors = "black",
          cc <- colors[sample(seq_along(colors), 1)]
         }
         text(x1, y1, words[i], cex = size[i], offset = 0, srt = rotWord * 90, col = cc)
-        boxes[[length(boxes) + 1]] <- c(x1 - .5 * wid, y1 - .5 * ht, wid, ht)
+        boxes[[length(boxes) + 1]] <- c(x1 - 0.5 * wid, y1 - 0.5 * ht, wid, ht)
         isOverlaped <- FALSE
       } else {
-        if(r > sqrt(.5)) {
+        if(r > sqrt(0.5)) {
           warning(words[i], "could not be fit on page. It will not be plotted.")
           isOverlaped <- FALSE
         }
         theta <- theta + thetaStep
-        r <- r + rStep * thetaStep / (2*pi)
-        x1 <- .5 + r * cos(theta)
-        y1 <- .5 + r * sin(theta)
+        r <- r + rStep * thetaStep / (2 * pi)
+        x1 <- 0.5 + r * cos(theta)
+        y1 <- 0.5 + r * sin(theta)
       }
     }
   }
